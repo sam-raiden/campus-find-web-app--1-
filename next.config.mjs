774123c880f7f972588bedca2b814bd14f4de0c4
Sam <thesamraiden@gmail.com>
2026-02-07 07:37:46 +0530

    last mj

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "nodejs", // 🔥 FORCE NODE (NO EDGE)
+  },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "https://lost-found-production-8253.up.railway.app/api/:path*",
+        destination:
+          "https://lost-found-production-8253.up.railway.app/api/:path*",
       },
     ];
   },
